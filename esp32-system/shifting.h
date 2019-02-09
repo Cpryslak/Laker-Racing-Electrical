07b16084e5d558f13f81fe22a1c4d0060751d22b
Corbin Bremmeyr <corbinbremmeyr@gmail.com>
2019-02-09 13:00:00 -0500

    Moved states enum and updateLeds() definetions to here

---
+++ esp32-system/shifting.h
@@ -0,0 +1,16 @@
+
+/** Structs and Enum Definitions */
+enum states{
+  first,
+  second,
+  third,
+  fourth,
+  fifth,
+  neutral
+};
+
+/** Header Funcion Definitions */
+#ifndef UPDATE_LEDS
+#define UPDATE_LEDS
+    void updateLeds();
+#endif
